347535dc5f2eb45fe01c3e239510659b9a7751a5
AlexM <a.morozov.work@mail.ru>
2020-09-11 23:08:24 +0300

    Index page images complete

---
+++ source/js/map.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  let pognalyMap;
+  let pognalyPlaceMark;
+
+  ymaps.ready({
+      successCallback: function () {
+        pognalyMap = new ymaps.Map("map", {
+              center: [59.938653, 30.323115],
+              zoom: 15,
+              controls: [],
+          },
+          {
+              suppressMapOpenBlock: true
+          },
+          {
+              yandexMapAutoSwitch: true
+          });
+
+          pognalyPlaceMark = new ymaps.Placemark(
+              [59.938653, 30.323115],
+          {
+              balloonContent: []
+          },
+          {
+              openBalloonOnClick: false,
+
+              iconLayout: 'default#image',
+
+              iconImageHref: './img/map-marker.svg',
+
+              iconImageSize: [50, 50],
+
+              iconImageOffset: [-25, -25]
+
+          });
+
+          pognalyMap.behaviors.disable('scrollZoom')
+          pognalyMap.geoObjects.add(pognalyPlaceMark);
+      }
+  });
+};
