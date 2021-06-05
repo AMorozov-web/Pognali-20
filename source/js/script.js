94d4928770a2c830bb780b6d873a38e77e3c7a2a
Alex Morozov <a.morozov.work@mail.ru>
2021-06-06 00:18:58 +0300

    feat: add catalog scripts

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,14 @@
 window.addEventListener(`DOMContentLoaded`, () => {
   const body = document.querySelector(`.page__body`);
   const header = document.querySelector(`.page-header`);
-  const navToggleButton = document.querySelector(`.nav-toggle-btn`);
   const rates = document.querySelector(`.rates`);
-  const ratesButton = document.querySelector(`.rates__modal-open`);
-  const ratesCloseButton = document.querySelector(`.business-rates__close`);
   const regFormInput = document.querySelector(`.reg-form__input`);
-  const regFormBtn = document.querySelector(`.reg-form__btn`);
+  const filterCountry = document.querySelector(`.filter-country`);
+  const hobbySelect = document.querySelector(`.hobby-select`);
+  const musicSelect = document.querySelector(`.music-select`);
+  const foodSelect = document.querySelector(`.food-select`);
+  const transportSelect = document.querySelector(`.transport-select`);
+  const levelSelect = document.querySelector(`.level-select`);
   const headerOffset = header.offsetTop;
 
   const validateEmail = (email) => {
@@ -18,7 +20,9 @@ window.addEventListener(`DOMContentLoaded`, () => {
     body.classList.remove(`page__body--no-js`);
   }
 
-  if (header && navToggleButton) {
+  if (header) {
+    const navToggleButton = document.querySelector(`.nav-toggle-btn`);
+
     navToggleButton.addEventListener(`click`, () => {
       header.classList.toggle(`page-header--opened-menu`);
     });
@@ -32,7 +36,10 @@ window.addEventListener(`DOMContentLoaded`, () => {
     });
   }
 
-  if (rates && ratesButton) {
+  if (rates) {
+    const ratesButton = document.querySelector(`.rates__modal-open`);
+    const ratesCloseButton = document.querySelector(`.business-rates__close`);
+
     ratesButton.addEventListener(`click`, (evt) => {
       evt.preventDefault();
       rates.classList.toggle(`rates--modal-open`);
@@ -44,6 +51,8 @@ window.addEventListener(`DOMContentLoaded`, () => {
   }
 
   if (regFormInput) {
+    const regFormBtn = document.querySelector(`.reg-form__btn`);
+
     regFormBtn.addEventListener(`click`, (evt) => {
       const email = regFormInput.value;
 
@@ -54,6 +63,61 @@ window.addEventListener(`DOMContentLoaded`, () => {
           regFormInput.classList.remove(`reg-form__input--invalid`);
         }, 2000);
       }
+    });
+  }
+
+  if (filterCountry) {
+    const filterCountryToggleBtn = document.querySelector(`.filter-country__toggle-btn`);
+
+    filterCountry.classList.remove(`filter-country--opened-menu`);
+
+    filterCountryToggleBtn.addEventListener(`click`, () => {
+      filterCountry.classList.toggle(`filter-country--opened-menu`);
+    });
+  }
+
+  if (hobbySelect) {
+    const button = document.querySelector(`.hobby-select__title-btn`);
+
+    hobbySelect.classList.remove(`hobby-select--opened-list`);
+    button.addEventListener(`click`, () => {
+      hobbySelect.classList.toggle(`hobby-select--opened-list`);
+    });
+  }
+
+  if (musicSelect) {
+    const button = document.querySelector(`.music-select__title-btn`);
+
+    musicSelect.classList.remove(`music-select--opened-list`);
+    button.addEventListener(`click`, () => {
+      musicSelect.classList.toggle(`music-select--opened-list`);
+    });
+  }
+
+  if (foodSelect) {
+    const button = document.querySelector(`.food-select__title-btn`);
+
+    foodSelect.classList.remove(`food-select--opened-list`);
+    button.addEventListener(`click`, () => {
+      foodSelect.classList.toggle(`food-select--opened-list`);
+    });
+  }
+
+  if (transportSelect) {
+    const button = document.querySelector(`.transport-select__title-btn`);
+
+    transportSelect.classList.remove(`transport-select--opened-list`);
+    button.addEventListener(`click`, () => {
+      transportSelect.classList.toggle(`transport-select--opened-list`);
+    });
+  }
+
+  if (levelSelect) {
+    const button = document.querySelector(`.level-select__title-btn`);
+
+    levelSelect.classList.remove(`level-select--opened-list`);
+    button.addEventListener(`click`, () => {
+      levelSelect.classList.toggle(`level-select--opened-list`);
     });
   }
 });
